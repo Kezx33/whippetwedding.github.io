1edc5e6ba67e1e2e0affea5c9bdce89e24a8474a
Kezx33 <95642973+Kezx33@users.noreply.github.com>
2024-05-07 14:59:00 +0100

    Add files via upload

--- JavaScript/RSVP.js
+++ JavaScript/RSVP.js
@@ -9,4 +9,36 @@ $(document).ready(function() {
   });
 });
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+$(document).ready(function(){
+  var $form = $('#nightForm');
+  $form.submit(function(){
+     $.post($(this).attr('action'), $(this).serialize(), function(response){
+           // do something here on success
+           $('#nightForm')[0].reset();
+           $("#ThankYou").toggle();
+           setTimeout(function(){location.reload(); },4000);
+     },'json');
+     return false;
+  });
+});
+
+
+$(document).ready(function(){
+  var $form = $('#dayForm');
+  $form.submit(function(){
+     $.post($(this).attr('action'), $(this).serialize(), function(response){
+           // do something here on success
+           $('#dayForm')[0].reset();
+           $("#ThankYou").toggle();
+           setTimeout(function(){location.reload(); },4000);
+     },'json');
+     return false;
+  });
+});
+
 
